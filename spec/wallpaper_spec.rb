0c2bbc910eab80cce9e5d0913e38e74c8a605d11
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-06-05 04:57:39 -0300

    change wallpaper test

--- spec/wallpaper_spec.rb
+++ spec/wallpaper_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Wallpaper" do
   it "expands the given path" do
-    wall = Wallpaper.new 'images/type_racer.png'
-    expect(wall.path).to be_eql('file://' + File.expand_path('images/type_racer.png'))
+    wall = Wallpaper.new 'images/foo.jpg'
+    expect(wall.path).to be_eql('file://' + File.expand_path('images/foo.jpg'))
   end
 end
